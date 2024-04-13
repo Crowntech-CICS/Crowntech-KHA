83cb2260203b7599052fc5af5b5c81e2ff9f6af2
jendrixcics <156883472+jendrixcics@users.noreply.github.com>
2024-04-13 13:30:22 +0800

    Add files via upload

--- src/java/controller/CreateAcc.java
+++ src/java/controller/CreateAcc.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import static org.apache.commons.lang3.RandomStringUtils.*;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
 
 
 public class CreateAcc extends HttpServlet {
@@ -16,9 +18,6 @@ public class CreateAcc extends HttpServlet {
     protected static ResultSet rs;
     protected static PreparedStatement ps;
     
-    protected String strkey; 
-	protected String encrpytKey = "RECORDKINGSVILLE";//getServletContext().getInitParameter("key");
-    protected String cipher = "AES/ECB/PKCS5Padding"; //getServletContext().getInitParameter("cipher");
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -31,30 +30,24 @@ public class CreateAcc extends HttpServlet {
     
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String encrpytKey = getServletContext().getInitParameter("key");
+        String cipher = getServletContext().getInitParameter("cipher");
         try {
             Class.forName(getServletContext().getInitParameter("jdbcClassName")); //load driver
             String username = getServletContext().getInitParameter("dbUserName"), //get connection parameters from web.xml
                    password = getServletContext().getInitParameter("dbPassword"),
                    driverURL = getServletContext().getInitParameter("jdbcDriverURL");
             con = DriverManager.getConnection(driverURL, username, password); //create connection
+        } catch (SQLException sqle) {
+            System.out.println("SQLException error occured - " + sqle.getMessage());
+        } catch (ClassNotFoundException nfe) {
+            System.out.println("ClassNotFoundException error occured - " + nfe.getMessage());
         }
-        catch (ClassNotFoundException | SQLException ex) {
-            System.out.println("A connection to the database could not be established.");
-            System.out.println("Error: " + ex.getMessage());
-
-        }
-    }
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-      
-		
-        
-        
-        
-        if (con != null) {
-            try {
+                int row;
                 String NewUserID = random(8,true,true);
 		String NewHomeOwnerID = ""; // if resident = needs homeownerid reference, if homeowner = generate new homeowner, if staff = "staffaydee"
                 String NewORNUM = (String) request.getParameter("ornum");;
@@ -64,46 +57,48 @@ public class CreateAcc extends HttpServlet {
                 String NewEmail = (String) request.getParameter("email");
                 String NewPass = (String) request.getParameter("password");
                 String NewRole = (String) request.getParameter("userrole"); // Roles are Homeowner, Resident, Staff
-                byte[] key = strkey.getBytes();
                 String EncryptPass = model.Encryption.encrypt(NewPass, encrpytKey, cipher);
                 HttpSession currentUser = request.getSession();
                 String dbQuery;
-                boolean uniqueID = false;
+                boolean uniqueUserID = false;
+                boolean uniqueHomeownerID = false;
+            try {
                 
-                do{
+                //Avoid duplicate userID
                 dbQuery = "SELECT USERID FROM USERS WHERE USERID = ?";
                 ps = con.prepareStatement(dbQuery);
+                do{
                 ps.setString(1, NewUserID);
                 rs = ps.executeQuery();
                 
                 if(rs.next())
                     NewUserID = random(8,true,true);
                 else
-                    uniqueID = true;
+                    uniqueUserID = true;
                 
-                } while (!uniqueID);
+                } while (!uniqueUserID);
                 
-                uniqueID = false;
                 
-		if (NewRole == "Homeowner")
+		if (NewRole.equals("Homeowner"))
 		{
 		NewHomeOwnerID = random(8,true,true);
                 
-                do{
+                //Avoiding Duplicate HomeownerID as homeOwner
                 dbQuery = "SELECT HOMEOWNERID FROM HOMEOWNER WHERE HOMEOWNERID = ?";
                 ps = con.prepareStatement(dbQuery);
+                do{
                 ps.setString(1, NewHomeOwnerID);
                 rs = ps.executeQuery();
                 
                 if(rs.next())
                     NewHomeOwnerID = random(8,true,true);
                 else
-                    uniqueID = true;
+                    uniqueHomeownerID = true;
                 
-                } while (!uniqueID);
+                } while (!uniqueHomeownerID);
                 
-		}
-		else if (NewRole == "Staff")
+                }
+		else if (NewRole.equals("Staff"))
 		{
 		NewHomeOwnerID = "staffaydee";
 		}
@@ -117,53 +112,75 @@ public class CreateAcc extends HttpServlet {
                 {
 		NewHomeOwnerID = rs.getString("HOMEOWNERID").trim() ;
                 }
-                
 		}
                 
-                dbQuery = "INSERT INTO LOGIN (USERID, EMAIL, PASSWORD) VALUES( ?, ?, ?)";
-                ps = con.prepareStatement(dbQuery);
-                ps.setString(1, NewUserID);
-                ps.setString(2, NewEmail);
-                ps.setString(3, EncryptPass);
-                int row = ps.executeUpdate(); //Create in Login DB
+                
+                if (NewRole.equals("Homeowner")){
+                String insHOQuery = "INSERT INTO HOMEOWNER (HOMEOWNERID, LASTNAME, FIRSTNAME, EMAIL, HOUSENO, STREETNAME, CITY, PROVINCE, PAID, BALANCE, ORNUM) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                ps = con.prepareStatement(insHOQuery);
+                ps.setString(1, NewHomeOwnerID);
+		ps.setString(2, NewLastname);
+		ps.setString(3, NewFirstname);
+                ps.setString(4, NewEmail);
+                ps.setString(5, "EMPTY");
+                ps.setString(6, "EMPTY");
+                ps.setString(7, "EMPTY");
+                ps.setString(8, "EMPTY");
+                ps.setBoolean(9, false); 
+                ps.setDouble(10, 0.0); 
+                ps.setString(11, NewORNUM); 
+                row = ps.executeUpdate();
+                if (row != 0) {                    
+                        System.out.println("Homeowner Created");
+                    }
+                     else {
+                         response.sendError(500);
+                    }
+                }
 				
-		dbQuery = "INSERT INTO USERS (USERID, HOMEOWNERID, LASTNAME, FIRSTNAME, RESIDENTCLASS, HOUSENO, STREETNAME, DATEOCCUPIED) VALUES( ?, ?, ?, ?, ?, ?)";
-                ps = con.prepareStatement(dbQuery);
+                
+                System.out.println(String.format("UserID: %s || Date Occupied: %s || Role: %s", NewUserID, NewDateoccupied, NewRole));
+                try{
+                java.util.Date myDate = new SimpleDateFormat("yyyy-MM-dd").parse(NewDateoccupied);
+                java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
+                
+		String insUserQuery = "INSERT INTO USERS (USERID, HOMEOWNERID, LASTNAME, FIRSTNAME, RESIDENTCLASS, HOUSENO, STREETNAME, DATEOCCUPIED) VALUES( ?, ?, ?, ?, ?, ?, ?, ?)";
+                ps = con.prepareStatement(insUserQuery);
                 ps.setString(1, NewUserID);
 		ps.setString(2, NewHomeOwnerID);
                 ps.setString(3, NewLastname);
                 ps.setString(4, NewFirstname);
                 ps.setString(5, NewRole); 
-                ps.setString(6, "<EMPTY>"); 
-                ps.setString(7, "<EMPTY>"); 
-                ps.setString(7, NewDateoccupied); 
+                ps.setString(6, "EMPTY"); 
+                ps.setString(7, "EMPTY"); 
+                ps.setDate(8, sqlDate);
                 row = ps.executeUpdate(); //Create in USERS DB
-                
-                if (NewRole == "Homeowner" ){
-                dbQuery = "INSERT INTO HOMEOWNER (HOMEOWNERID, LASTNAME, FIRSTNAME, EMAIL, CITY, PROVINCE, PAID, BALANCE, ORNUM) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                ps = con.prepareStatement(dbQuery);
-                ps.setString(1, NewHomeOwnerID);
-		ps.setString(2, NewLastname);
-		ps.setString(3, NewFirstname);
-                ps.setString(4, NewEmail);
-                ps.setString(6, "<EMPTY>");
-                ps.setString(7, "<EMPTY>");
-                ps.setString(5, "FALSE"); 
-                ps.setString(5, "0"); 
-                ps.setString(6, NewORNUM); 
-                row = ps.executeUpdate();
-                }
-                     if (row != 0) {                    
-                    
-                    
-                         response.sendRedirect("createacc.jsp");
-                    
+                    if (row != 0) {                    
+                        System.out.println("User Created");
                     }
-                
                      else {
-                         
                          response.sendError(500);
-                    
+                    }
+                }
+                catch (Exception e){
+                System.out.println("Failed to Parse");
+                }
+                
+                System.out.println(String.format("UserID: %s || Password: %s || Role: %s", NewUserID, EncryptPass, NewRole));
+                
+                String insLogQuery = "INSERT INTO LOGIN (USERID, EMAIL, PASSWORD) VALUES( ?, ?, ?)";
+                ps = con.prepareStatement(insLogQuery);
+                ps.setString(1, NewUserID);
+                ps.setString(2, NewEmail);
+                ps.setString(3, EncryptPass);
+                row = ps.executeUpdate(); //Create in Login DB
+                
+                     if (row != 0) {
+                         System.out.println("Login Created");
+                         response.sendRedirect("createacc.jsp");
+                    }
+                     else {
+                         response.sendError(500);
                     }
                 }
                 
@@ -186,7 +203,7 @@ public class CreateAcc extends HttpServlet {
 			
 			
         }
-    }
+    
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
